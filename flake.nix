a9dc04b0170394240be44caa04d047b4f90795ea
rudesome <26667983+rudesome@users.noreply.github.com>
2026-05-02 20:14:13 +0200

    chore: change input to 25.11 (stable)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "rudesome neovim configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
